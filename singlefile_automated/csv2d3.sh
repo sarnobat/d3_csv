2727f727c6b037bcc9ffc62d175401389e3857db
Sridhar Sarnobat <ss401533@gmail.com>
2016-12-06 21:03:23 -0800

    Minor changes (untested)

--- singlefile_automated/csv2d3.sh
+++ singlefile_automated/csv2d3.sh
@@ -1,3 +1,3 @@
 cat before.html | tee out.html
-cat - | perl -pe 's{"?([^"]*)"?,"?([^"]*)"?}{  \{ source : "$1", target : "$2" \},}g' | tee -a out.html
+cat - | grep -v "source,target" | perl -pe 's{"?([^"]*)"?,"?([^"]*)"?}{  \{ source : "$1", target : "$2" \},}g' | tee -a out.html
 cat after.html | tee -a out.html
--- singlefile_automated/run.sh
+++ singlefile_automated/run.sh
@@ -1,1 +1,2 @@
 cat minimal.csv | sh csv2d3.sh
+#cat ~/work/csv/saas.csv | sh csv2d3.sh
